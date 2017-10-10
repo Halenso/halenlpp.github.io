c8fdf162cede4f36605ec193d1df7ebd758bcbaf
Halenso <32223122+Halenso@users.noreply.github.com>
2017-10-10 11:09:16 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -97,28 +97,24 @@ function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
-	document.write(seconds);
-	document.write("+");
+	document.write(Date.parse(current));
+	document.write("-");
 	seconds = seconds % (3600 * 24);
-	document.write(seconds);
-	document.write("+");
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
-	document.write(seconds);
-	document.write("+");
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
 	}
 	seconds = seconds % 60;
-	document.write(seconds);
+	document.write(Date.parse(date));
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	document.write("一个循环");
+	document.write("= 一个循环");
 	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
 }
