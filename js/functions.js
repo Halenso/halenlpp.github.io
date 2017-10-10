06eb365a7741ca7d547fb7564afa301ec218f1c9
Halenso <32223122+Halenso@users.noreply.github.com>
2017-10-10 11:01:10 +0800

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -98,14 +98,17 @@ function timeElapse(date){
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
 	var days = Math.floor(seconds / (3600 * 24));
 	document.write(seconds);
+	document.write("+");
 	seconds = seconds % (3600 * 24);
 	document.write(seconds);
+	document.write("+");
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
 		hours = "0" + hours;
 	}
 	seconds = seconds % 3600;
 	document.write(seconds);
+	document.write("+");
 	var minutes = Math.floor(seconds / 60);
 	if (minutes < 10) {
 		minutes = "0" + minutes;
